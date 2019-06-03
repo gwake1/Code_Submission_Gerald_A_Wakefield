f7eb6b6b83665a10869ad9bc2fd4527d505021f2
Gerald Anthony Wakefield <gwake1@gmail.com>
2019-06-03 13:28:13 -0500

    refactor and update code

--- Code_Submission_Gerald_A_Wakefield/Facade/CoFactorFacade.cs
+++ Code_Submission_Gerald_A_Wakefield/Facade/CoFactorFacade.cs
@@ -1,6 +1,7 @@
 ﻿using Code_Submission_Gerald_A_Wakefield.Configuration;
 using Code_Submission_Gerald_A_Wakefield.Interface;
 using Code_Submission_Gerald_A_Wakefield.Model;
+using Code_Submission_Gerald_A_Wakefield.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +10,23 @@ namespace Code_Submission_Gerald_A_Wakefield.Facade
 {
     public class CoFactorFacade
     {
+        readonly IUtil _util;
         readonly IInputService _inputService;
         readonly IFactorService _factorService;
         private List<Factor> factors = new List<Factor>();
 
         public CoFactorFacade()
         {
+            _util = ServiceInjector.Retrieve<IUtil>();
             _inputService = ServiceInjector.Retrieve<IInputService>();
             _factorService = ServiceInjector.Retrieve<IFactorService>();
         }
 
-        public CoFactorFacade(IInputService inputService, IFactorService factorService)
+        public CoFactorFacade(IUtil util, IInputService inputService, IFactorService factorService)
         {
-            _inputService = inputService;
-            _factorService = factorService;
+            _util = util;
+            _inputService = new InputService(_util);
+            _factorService = new FactorService(_util);
         }
 
         public bool Load(string input)
@@ -30,7 +34,11 @@ namespace Code_Submission_Gerald_A_Wakefield.Facade
             if (factors.Count < 2)
             {
                 var val = _inputService.ReadInt(input);
-                factors.Add(new Factor(val, _factorService.getSum(val)));
+
+                factors.Add(new Factor(_util, val, _factorService.getSum(val)));
+            }
+            if (factors.Count < 2)
+            {
                 return true;
             }
             return false;
@@ -43,7 +51,15 @@ namespace Code_Submission_Gerald_A_Wakefield.Facade
                 throw new InvalidOperationException("The CoFactor Facade has not been populated with enough Factors");
             }
             var sum = factors.Select(x => x.Sum).Sum();
-            var commonVals = _factorService.getSum(LeastCommonMultiple(GreatestCommonDivisor()));
+
+            var inputs = factors.Select(x => x.Value).ToList();
+            var GCD = GreatestCommonDivisor();
+            var commonVals = 0;
+            var LCD = LeastCommonMultiple(GCD);
+            if (LCD < _util.MaxValue || GCD != 1)
+            {
+                commonVals = _factorService.getSum(LCD);
+            }
             cleanUp();
             return sum - commonVals;
         }
--- TestProject/Facade/CoFactorFacade.cs
+++ TestProject/Facade/CoFactorFacade.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using Code_Submission_Gerald_A_Wakefield.Interface;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
 using TestProject.TestClassBuilder;
 
 namespace TestProject.Facade
@@ -8,13 +10,16 @@ namespace TestProject.Facade
     public class CoFactorFacade
     {
         [TestMethod]
-        public void CoFactorFacade_InitialCondition_ExpectedResult()
+        public void CoFactorFacade_WithValues3_5_ANDMaxValue1000_ExpectedResult233168()
         {
             //Arrange
-            var facade = new CoFactorFacadeTestClassBuilder().Build();
+            var expected = 233168;
+            var maxValue = 1000;
+            var minSum = (maxValue / 2) - 1;
+            var util = Mock.CreateLike<IUtil>(config => config.MaxValue == maxValue && config.MaxInputValue == maxValue - 1 && config.MinSum == minSum);
+            var facade = new CoFactorFacadeTestClassBuilder().WithUtil(util).Build();
             facade.Load("3");
             facade.Load("5");
-            var expected = 233168;
 
             //Act
             var result = facade.Calculate();
@@ -24,7 +29,27 @@ namespace TestProject.Facade
         }
 
         [TestMethod]
-        public void CoFactorFacade_InvalidLoadAttempts_ReturnsInvalidOperationException()
+        public void CoFactorFacade_WithValues3_5_ANDMaxValue10_ExpectedResult23()
+        {
+            //Arrange
+            var expected = 23;
+            var maxValue = 10;
+            var minSum = (maxValue / 2) - 1;
+            var util = Mock.CreateLike<IUtil>(config => config.MaxValue == maxValue && config.MaxInputValue == maxValue - 1 && config.MinSum == minSum);
+            var facade = new CoFactorFacadeTestClassBuilder().WithUtil(util).Build();
+            facade.Load("3");
+            facade.Load("5");
+
+            //Act
+            var result = facade.Calculate();
+
+            //Assert
+            result.Should().Be(expected);
+
+        }
+
+        [TestMethod]
+        public void CoFactorFacade_InvalidLoadAttempts_ReturnsFalse()
         {
             //Arrange
             var facade = new CoFactorFacadeTestClassBuilder().Build();
