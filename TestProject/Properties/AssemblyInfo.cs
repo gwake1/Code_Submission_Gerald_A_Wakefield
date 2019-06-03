c829a7778a14acb85b9fca4b11a6122c3cf5160f
Gerald Anthony Wakefield <gwake1@gmail.com>
2019-06-03 06:04:01 -0500

    Init Commit with first tests for UI Input

---
+++ TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5789bf6c-fe6d-4801-8a89-0e7b37d23021")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProject/UI_Input.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UI_Input
+    {
+
+        [TestMethod]
+        public void ReadInt_WithValidString_ReturnsAnInt()
+        {
+            //Arrange
+            var service = new InputService();
+            var validNum = "7";
+
+            //Act
+            var result = service.ReadInt(validNum);
+
+            //Assert
+            result.Should().BeOfType(typeof(int), "because the valid inputs prompted to the user will be positive whole-number integers");
+        }
+
+        [TestMethod]
+        public void ReadInt_WithValidStringValue7_ReturnsIntValue7()
+        {
+            //Arrange
+            var service = new InputService();
+            var validNum = "7";
+
+            //Act
+            var result = service.ReadInt(validNum);
+
+            //Assert
+            result.Should().Be(7, "because the console reads in user input as a string that must be converted to an integer");
+        }
+
+        [TestMethod]
+        public void ReadInt_WithNegativeString_ReturnsArgumentException()
+        {
+            //Arrange
+            var service = new InputService();
+            var invalidNum = "-1";
+
+            //Act
+            Action action = () => service.ReadInt(invalidNum);
+
+            //Assert
+            action.Should().Throw<ArgumentException>().WithMessage("Please provide a Numeric Value greater than Zero");
+        }
+
+        [TestMethod]
+        public void ReadInt_WithInvalidCharString_ReturnsArgumentException()
+        {
+            //Arrange
+            var service = new InputService();
+            var invalidString = "Hello";
+
+            //Act
+            Action action = () => service.ReadInt(invalidString);
+
+            //Assert
+            action.Should().Throw<ArgumentException>().WithMessage("Please Provide Only One Numeric Values");
+        }
+
+        [TestMethod]
+        public void ReadInt_WithMultipleInts_ReturnsArgumentException()
+        {
+            //Arrange
+            var service = new InputService();
+            var invalidInts = "1 2 3";
+
+            //Act
+            Action action = () => service.ReadInt(invalidInts);
+
+            //Assert
+            action.Should().Throw<ArgumentException>().WithMessage("Please Provide Only One Numeric Values");
+        }
+
+        [TestMethod]
+        public void ReadInt_WithEmptyString_ReturnsArgumentNullException()
+        {
+            //Arrange
+            var service = new InputService();
+            var emptyString = String.Empty;
+
+            //Act
+            Action action = () => service.ReadInt(emptyString);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
